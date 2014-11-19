2e89337fa52a904cf946058288332175157e8802
Wenting Rohwer <wenting.rohwer@gmail.com>
2014-11-19 12:24:16 -0800

    test

---
+++ pages/myreview_copy/javascript/myreview.js
@@ -0,0 +1,17 @@
+$('#menu').on('click', function (){
+  $('nav').animate({width: 'toggle'}, 250);
+});
+
+$('.reviewTitle').on('click', function () {
+  $(this).find('.comment').slideToggle();
+  if ($(this).css('opacity') != '1') {
+    $(this).css('opacity', '1');
+  }
+  else {
+    $(this).css('opacity', '0.8');
+  }
+});
+
+$('#pageTitle').on('click', function() {
+  $('#profileInfo').slideToggle();
+});
